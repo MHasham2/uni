024e189c81f07fe968c35788c607272e849e1a8d
MHasham2 <hishamnizamani356@gmail.com>
2026-03-04 16:42:13 +0500

    Add files via upload

---
+++ 1year/sir-umair/Find-Average-of-Three-Numbers.cpp
@@ -0,0 +1,19 @@
+
+////Name:hasham
+///roll:53
+///class:CS I (B)
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 24, m = 48, o = 12;
+    float average;
+
+    average = (n + m + o) / 3.0;
+
+    cout << "Average is: " << average << endl;
+
+    return 0;
+}
